30edd23d41c5044e4b88ecfb3b69e90b62abc41e
unknown <yudhistiray198@gmail.com>
2020-09-01 08:52:09 +0700

    add assert

--- lib/ridjnaelcrypt.dart
+++ lib/ridjnaelcrypt.dart
@@ -4,14 +4,13 @@ import 'dart:convert';
 import 'package:crypto/crypto.dart' as crypt;
 import 'package:steel_crypt/steel_crypt.dart';
 
-
 /// Ridjnael Crypt
 class Ridjnael {
-
   static String setKey;
   static String setIv;
 
-  static String computeDecrypt(String encryptedText){
+  static String computeDecrypt(String encryptedText) {
+    assert(setKey != null || setIv != null);
 
     try {
       var keys = utf8.encode(setKey);
@@ -19,27 +18,30 @@ class Ridjnael {
       var finalIV = crypt.md5.convert(ivLocal);
       var finalKeys = crypt.sha256.convert(keys);
 
-      return AesCrypt.computeRijndaelDecrypt(encryptedText, finalIV.bytes, finalKeys.bytes);
-    }
-    catch(ex){
-     return ex.toString();
+      return AesCrypt.computeRijndaelDecrypt(
+          encryptedText, finalIV.bytes, finalKeys.bytes);
+    } catch (ex) {
+      return ex.toString();
     }
   }
 
-  static String computeEncrypt(String plainText){
-   try{
-     var keys = utf8.encode(setKey);
-     var ivLocal = utf8.encode(setIv);
-     var finalIV = crypt.md5.convert(ivLocal);
-     var finalKeys = crypt.sha256.convert(keys);
+  static String computeEncrypt(String plainText) {
+    assert(setKey != null || setIv != null);
 
-     return AesCrypt.computeRijndaelEncrypt(plainText, finalIV.bytes, finalKeys.bytes);
-   } catch(ex) {
-     return ex.toString();
-   }
+    try {
+      var keys = utf8.encode(setKey);
+      var ivLocal = utf8.encode(setIv);
+      var finalIV = crypt.md5.convert(ivLocal);
+      var finalKeys = crypt.sha256.convert(keys);
+
+      return AesCrypt.computeRijndaelEncrypt(
+          plainText, finalIV.bytes, finalKeys.bytes);
+    } catch (ex) {
+      return ex.toString();
+    }
   }
 
-  static String computeSHA1(String input){
+  static String computeSHA1(String input) {
     var a = ascii.encode(input);
     var s = crypt.sha1.convert(a);
 
